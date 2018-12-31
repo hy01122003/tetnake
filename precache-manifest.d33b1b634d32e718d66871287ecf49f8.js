68a924be970015556b8193c9c41a73d62b5ade43
mysite <hoanghy01122003@gmail.com>
2018-12-31 09:45:07 +0700

    Updates

---
+++ precache-manifest.d33b1b634d32e718d66871287ecf49f8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "caaab5aaa6c5cfa93b47",
+    "url": "/tetnake/static/css/main.4463c77f.chunk.css"
+  },
+  {
+    "revision": "caaab5aaa6c5cfa93b47",
+    "url": "/tetnake/static/js/main.caaab5aa.chunk.js"
+  },
+  {
+    "revision": "a909a361f4e44e778ce3",
+    "url": "/tetnake/static/css/1.5b271ad7.chunk.css"
+  },
+  {
+    "revision": "a909a361f4e44e778ce3",
+    "url": "/tetnake/static/js/1.a909a361.chunk.js"
+  },
+  {
+    "revision": "384968bfaa2d158b1bc4",
+    "url": "/tetnake/static/js/runtime~main.384968bf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tetnake/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "579b452d5c6f2cce72563e7fd4826af3",
+    "url": "/tetnake/index.html"
+  }
+];
